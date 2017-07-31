85a445ec2930c8e3c4ec51846021fdbb5dfc1f3c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-31 12:19:52 -0700

    Added reminder notes on JSON transmission.

--- Ajax.js
+++ Ajax.js
@@ -9,3 +9,14 @@ request.onreadystatechange = function () {
 
 // Add the code to open the AJAX request using the GET method and pointing to the 'footer.html' file.
 request.open('GET', 'footer.html');
+request.send();
+
+
+// reminder, JSONP = Json with Padding
+// a web proxy is useful for AJAX requests because it allows you to bypass a web 
+// browser's same-origin policy by retrieving data from another server while keeping 
+// AJAX requests within the same domain.
+
+
+// JSON is transmitted over the web as plain text. To make it useful for a 
+// JavaScript program, you need to parse it, or convert it from a string to JavaScript.
