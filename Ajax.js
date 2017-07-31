10367b4fabae9793f8809e47fad30268af60e798
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-31 11:59:30 -0700

    Practice with AJAX and JSON.

---
+++ Ajax.js
@@ -0,0 +1,8 @@
+// practice with AJAX and JSON
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+  if (request.readyState === 4) {
+    document.getElementById("footer").innerHTML = request.responseText;
+  }
+};
